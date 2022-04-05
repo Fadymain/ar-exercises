47ef8edf3944e2dccde72fe2062991e6687643ca
Adetola Fadairo <adetolafadairo@gmail.com>
2022-04-05 20:18:21 +0000

    finding store 1 & 2 bu id and reassigning stores 1's name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+puts "annual revenue", @store1.annual_revenue
+@store1.name = "CheapStore"
+@store1.save
+puts "Store name", @store1.name
+
+@store2 = Store.find(2)
+
+
+
+# Store.find_by(name: "Richmond")
